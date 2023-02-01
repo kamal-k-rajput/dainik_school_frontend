f2a9ee0cd52809d857e61a2485868b634bfc887a
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2023-02-02 01:46:36 +0530

    change in course page

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,13 +1,44 @@
 import React from "react";
 import { Footer } from "../Footer/Footer";
 import courses_top_image from "../../ASSETS/IMG/courses/courses_top_image.png";
+import hariyana_ninth_english from "../../ASSETS/IMG/courses/hariyana_ninth_english.png";
 import "./Courses.css";
 export const Courses = () => {
+  const courses = [
+    {
+      img: hariyana_ninth_english,
+      title: "NIOS 12TH ENGLISH",
+      price: "",
+      rating: "",
+    },
+  ];
   return (
     <>
       <div className="courses_container">
         <div className="all_courses">
           <img src={courses_top_image} alt="dainik school courses" />
+        </div>
+        <section className="courses_section">
+          <h2>Do it now</h2>
+          <h2>
+            Grow your
+            <span className="mid_text"> Careers</span>
+          </h2>
+        </section>
+        <div className="courses_list">
+          {courses.forEach((course) => {
+            return (
+              <div>
+                <img src={course.img} alt="hariyana ninth" />
+                <p>Rating</p>
+                <p>{course.title}</p>
+                <div>
+                  <span>1199.99-/</span>
+                  <button>Add to Cart</button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
       <Footer />
