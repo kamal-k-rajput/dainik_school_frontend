cefa78e9a166fcd0c833a64fe4ad2a5a40738a71
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2023-03-01 23:29:59 +0530

    abb box in course pages

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -4,43 +4,25 @@ import courses_top_image from "../../ASSETS/IMG/courses/courses_top_image.png";
 import hariyana_ninth_english from "../../ASSETS/IMG/courses/hariyana_ninth_english.png";
 import "./Courses.css";
 export const Courses = () => {
-  const courses = [
-    {
-      img: hariyana_ninth_english,
-      title: "NIOS 12TH ENGLISH",
-      price: "",
-      rating: "",
-    },
-  ];
   return (
     <>
       <div className="courses_container">
         <div className="all_courses">
           <img src={courses_top_image} alt="dainik school courses" />
         </div>
-        <section className="courses_section">
-          <h2>Do it now</h2>
-          <h2>
-            Grow your
-            <span className="mid_text"> Careers</span>
-          </h2>
+        <section className="courses-section">
+          <div>course</div>
+          <div>course</div>
+          <div>course</div>
+          <div>course</div>
+          <div>course</div>
+          <div>course</div>
+          <div>course</div>
+          <div>course</div>
+          <div>course</div>
         </section>
-        <div className="courses_list">
-          {courses.forEach((course) => {
-            return (
-              <div>
-                <img src={course.img} alt="hariyana ninth" />
-                <p>Rating</p>
-                <p>{course.title}</p>
-                <div>
-                  <span>1199.99-/</span>
-                  <button>Add to Cart</button>
-                </div>
-              </div>
-            );
-          })}
-        </div>
       </div>
+
       <Footer />
     </>
   );
