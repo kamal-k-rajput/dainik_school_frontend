86ebdd4e72b233f52a71a19521ec8ce7041e7082
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2023-04-13 02:17:11 +0530

    add top margin in about us

--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { CustomHeader } from "../Tools/CustomHeader";
 import { Footer } from "../Footer/Footer";
 import { MoveToTop } from "../Tools/MoveToTop";
+import { Gap } from "../Tools/Gap";
 
 export const AboutUs = () => {
   return (
-    <div>
+    <>
+      <Gap/>
       <CustomHeader props={{ title: "About us" }} />
       AboutUs
       <Footer />
       <MoveToTop />
-    </div>
+    </>
   );
 };
