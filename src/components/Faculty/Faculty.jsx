8315f9aafeb93f3a734cd00a671f3322a99e3783
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2023-04-20 09:07:32 +0530

    new instructor added

--- src/components/Faculty/Faculty.jsx
+++ src/components/Faculty/Faculty.jsx
@@ -4,6 +4,7 @@ import { WhatsMore } from "../Home/WhatsMore/WhatsMore";
 import { Gap } from "../Tools/Gap";
 import HorizoantalLine from "../HorizoantalLine/HorizoantalLine";
 import praveen from "../../ASSETS/IMG/homepage/Instructor/instructor.jpg";
+import dkgupta from "../../ASSETS/IMG/homepage/Instructor/dkgupta.jpeg";
 import vandna from "../../ASSETS/IMG/homepage/Instructor/vandna.jpg";
 import { Footer } from "../Footer/Footer";
 import { CustomHeader } from "../Tools/CustomHeader";
@@ -35,15 +36,15 @@ export const Faculty = () => {
             link: "/course",
           }}
         />
-        {/* <VerticalCards
+        <VerticalCards
           cardProps={{
-            img: "team",
-            title: "School ",
-            text: "",
-            buttonLabel: "Join the team",
+            img: dkgupta,
+            title: "D.K. Gupta ",
+            text: "Chemistry NEET/IIT",
+            buttonLabel: "",
             link: "/course",
           }}
-        /> */}
+        />
       </div>
       <Footer />
       <MoveToTop />
