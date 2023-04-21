887a379d36df9bbf0dee74d11e967c2d1feb9b83
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2023-04-21 21:38:02 +0530

    dkgupta image format

--- src/components/Faculty/Faculty.jsx
+++ src/components/Faculty/Faculty.jsx
@@ -4,7 +4,7 @@ import { WhatsMore } from "../Home/WhatsMore/WhatsMore";
 import { Gap } from "../Tools/Gap";
 import HorizoantalLine from "../HorizoantalLine/HorizoantalLine";
 import praveen from "../../ASSETS/IMG/homepage/Instructor/instructor.jpg";
-import dkgupta from "../../ASSETS/IMG/homepage/Instructor/dkgupta.jpeg";
+import dkgupta from "../../ASSETS/IMG/homepage/Instructor/dkgupta.jpg";
 import vandna from "../../ASSETS/IMG/homepage/Instructor/vandna.jpg";
 import { Footer } from "../Footer/Footer";
 import { CustomHeader } from "../Tools/CustomHeader";
